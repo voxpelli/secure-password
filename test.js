d1f7e4b77a5d5155b5b7ae2f5d0cce092a3bc19e
Pelle Wessman <pelle@kodfabrik.se>
2025-07-01 14:24:53 +0200

    test: manual improvements to style and types

--- test.js
+++ test.js
@@ -7,38 +7,41 @@ import { SecurePassword } from './index.js'
 
 test('Can hash password sync', /** @param {TestContext} t */ (t) => {
   const pwd = new SecurePassword({
-    version: 0,
     memlimit: SecurePassword.MEMLIMIT_DEFAULT,
     opslimit: SecurePassword.OPSLIMIT_DEFAULT,
   })
+
   const userPassword = Buffer.from('my secrets')
   const passwordHash = pwd.hashSync(userPassword)
+
   t.assert.ok(!userPassword.equals(passwordHash))
 })
 
 test('Can hash password using promises', /** @param {TestContext} t */ async (t) => {
   const pwd = new SecurePassword({
-    version: 0,
     memlimit: SecurePassword.MEMLIMIT_DEFAULT,
     opslimit: SecurePassword.OPSLIMIT_DEFAULT,
   })
+
   const userPassword = Buffer.from('my secrets')
   const passwordHash = await pwd.hash(userPassword)
+
   t.assert.ok(!userPassword.equals(passwordHash))
 })
 
 test('Can hash password async simultaneous', /** @param {TestContext} t */ async (t) => {
   t.plan(4)
   const pwd = new SecurePassword({
-    version: 0,
     memlimit: SecurePassword.MEMLIMIT_DEFAULT,
     opslimit: SecurePassword.OPSLIMIT_DEFAULT,
   })
+
   const userPassword = Buffer.from('my secrets')
   const [hash1, hash2] = await Promise.all([
     pwd.hash(userPassword),
     pwd.hash(userPassword),
   ])
+
   t.assert.ok(!userPassword.equals(hash1))
   t.assert.ok(!userPassword.equals(hash2))
   t.assert.ok(Buffer.isBuffer(hash1))
@@ -47,24 +50,26 @@ test('Can hash password async simultaneous', /** @param {TestContext} t */ async
 
 test('Can verify password (identity) sync', /** @param {TestContext} t */ (t) => {
   const pwd = new SecurePassword({
-    version: 0,
     memlimit: SecurePassword.MEMLIMIT_DEFAULT,
     opslimit: SecurePassword.OPSLIMIT_DEFAULT,
   })
+
   const userPassword = Buffer.from('my secret')
   const passwordHash = pwd.hashSync(userPassword)
+
   t.assert.ok(pwd.verifySync(userPassword, passwordHash) === SecurePassword.VALID)
 })
 
 test('Can verify password (identity) using promises', /** @param {TestContext} t */ async (t) => {
   const pwd = new SecurePassword({
-    version: 0,
     memlimit: SecurePassword.MEMLIMIT_DEFAULT,
     opslimit: SecurePassword.OPSLIMIT_DEFAULT,
   })
+
   const userPassword = Buffer.from('my secret')
   const passwordHash = await pwd.hash(userPassword)
   const bool = await pwd.verify(userPassword, passwordHash)
+
   t.assert.ok(bool === SecurePassword.VALID)
 })
 
@@ -73,6 +78,7 @@ test('Needs rehash sync', /** @param {TestContext} t */ (t) => {
     memlimit: SecurePassword.MEMLIMIT_DEFAULT,
     opslimit: SecurePassword.OPSLIMIT_DEFAULT,
   })
+
   const userPassword = Buffer.from('my secret')
   const wrongPassword = Buffer.from('my secret 2')
   const pass = Buffer.from('hello world')
@@ -81,35 +87,49 @@ test('Needs rehash sync', /** @param {TestContext} t */ (t) => {
   const argon2ipassempty = Buffer.from('JGFyZ29uMmkkdj0xOSRtPTMyNzY4LHQ9NCxwPTEkN3dZV0EvbjBHQjRpa3lwSWN5UVh6USRCbjd6TnNrcW03aWNwVGNjNGl6WC9xa0liNUZBQnZVNGw2MUVCaTVtaWFZAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=', 'base64')
   const weakHash = weakPwd.hashSync(userPassword)
   const weakValid = weakPwd.verifySync(userPassword, weakHash)
+
   t.assert.ok(weakValid === SecurePassword.VALID)
   t.assert.ok(weakValid !== SecurePassword.INVALID)
   t.assert.ok(weakValid !== SecurePassword.VALID_NEEDS_REHASH)
+
   const weakInvalid = weakPwd.verifySync(wrongPassword, weakHash)
+
   t.assert.ok(weakInvalid !== SecurePassword.VALID)
   t.assert.ok(weakInvalid === SecurePassword.INVALID)
   t.assert.ok(weakInvalid !== SecurePassword.VALID_NEEDS_REHASH)
+
   const betterPwd = new SecurePassword({
     memlimit: SecurePassword.MEMLIMIT_DEFAULT + 1024,
     opslimit: SecurePassword.OPSLIMIT_DEFAULT + 1,
   })
+
   const rehashValid = betterPwd.verifySync(userPassword, weakHash)
+
   t.assert.ok(rehashValid !== SecurePassword.VALID)
   t.assert.ok(rehashValid !== SecurePassword.INVALID)
   t.assert.ok(rehashValid === SecurePassword.VALID_NEEDS_REHASH)
+
   const rehashValidAlgo = betterPwd.verifySync(pass, argon2ipass)
+
   t.assert.ok(rehashValidAlgo !== SecurePassword.VALID)
   t.assert.ok(rehashValidAlgo !== SecurePassword.INVALID)
   t.assert.ok(rehashValidAlgo === SecurePassword.VALID_NEEDS_REHASH)
+
   const rehashValidAlgoEmpty = betterPwd.verifySync(empty, argon2ipassempty)
+
   t.assert.ok(rehashValidAlgoEmpty !== SecurePassword.VALID)
   t.assert.ok(rehashValidAlgoEmpty !== SecurePassword.INVALID)
   t.assert.ok(rehashValidAlgoEmpty === SecurePassword.VALID_NEEDS_REHASH)
+
   const betterHash = betterPwd.hashSync(userPassword)
   const betterValid = betterPwd.verifySync(userPassword, betterHash)
+
   t.assert.ok(betterValid === SecurePassword.VALID)
   t.assert.ok(betterValid !== SecurePassword.INVALID)
   t.assert.ok(betterValid !== SecurePassword.VALID_NEEDS_REHASH)
+
   const betterInvalid = betterPwd.verifySync(wrongPassword, betterHash)
+
   t.assert.ok(betterInvalid !== SecurePassword.VALID)
   t.assert.ok(betterInvalid === SecurePassword.INVALID)
   t.assert.ok(betterInvalid !== SecurePassword.VALID_NEEDS_REHASH)
@@ -117,9 +137,12 @@ test('Needs rehash sync', /** @param {TestContext} t */ (t) => {
 
 test('Can handle invalid hash sync', /** @param {TestContext} t */ (t) => {
   const pwd = new SecurePassword()
+
   const userPassword = Buffer.from('my secret')
   const invalidHash = Buffer.allocUnsafe(SecurePassword.HASH_BYTES)
+
   const unrecognizedHash = pwd.verifySync(userPassword, invalidHash)
+
   t.assert.ok(unrecognizedHash === SecurePassword.INVALID_UNRECOGNIZED_HASH)
   t.assert.ok(unrecognizedHash !== SecurePassword.INVALID)
   t.assert.ok(unrecognizedHash !== SecurePassword.VALID)
@@ -128,9 +151,12 @@ test('Can handle invalid hash sync', /** @param {TestContext} t */ (t) => {
 
 test('Can handle invalid hash async', /** @param {TestContext} t */ async (t) => {
   const pwd = new SecurePassword()
+
   const userPassword = Buffer.from('my secret')
   const invalidHash = Buffer.allocUnsafe(SecurePassword.HASH_BYTES)
+
   const unrecognizedHash = await pwd.verify(userPassword, invalidHash)
+
   t.assert.ok(unrecognizedHash === SecurePassword.INVALID_UNRECOGNIZED_HASH)
   t.assert.ok(unrecognizedHash !== SecurePassword.INVALID)
   t.assert.ok(unrecognizedHash !== SecurePassword.VALID)
