db88ea99618bf9382ea91dc3a564bd5528fe38cf
Pelle Wessman <pelle@kodfabrik.se>
2025-07-01 15:25:36 +0200

    refactor!: export constants from package instead

--- index.js
+++ index.js
@@ -7,20 +7,39 @@ const cryptoPwhashStrAsync = promisify(sodium.crypto_pwhash_str_async)
 const cryptoPwhashStrVerifyAsync = promisify(sodium.crypto_pwhash_str_verify_async)
 const cryptoPwhashStrNeedsRehash = sodium.crypto_pwhash_str_needs_rehash
 
+// Sodium constants
+
+export const HASH_BYTES = sodium.crypto_pwhash_STRBYTES
+export const PASSWORD_BYTES_MIN = sodium.crypto_pwhash_PASSWD_MIN
+export const PASSWORD_BYTES_MAX = sodium.crypto_pwhash_PASSWD_MAX
+export const MEMLIMIT_MIN = sodium.crypto_pwhash_MEMLIMIT_MIN
+export const MEMLIMIT_MAX = sodium.crypto_pwhash_MEMLIMIT_MAX
+export const OPSLIMIT_MIN = sodium.crypto_pwhash_OPSLIMIT_MIN
+export const OPSLIMIT_MAX = sodium.crypto_pwhash_OPSLIMIT_MAX
+export const MEMLIMIT_DEFAULT = sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE
+export const OPSLIMIT_DEFAULT = sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE
+
+// Symbols
+
+export const INVALID = Symbol('INVALID')
+export const INVALID_UNRECOGNIZED_HASH = Symbol('INVALID_UNRECOGNIZED_HASH')
+export const VALID = Symbol('VALID')
+export const VALID_NEEDS_REHASH = Symbol('VALID_NEEDS_REHASH')
+
+/** @typedef {typeof INVALID | typeof INVALID_UNRECOGNIZED_HASH | typeof VALID | typeof VALID_NEEDS_REHASH} VerificationResult */
+
 /**
  * Checks if a hash buffer uses a recognized Argon2 algorithm.
  *
  * @param {Buffer} hashBuf
  * @returns {boolean}
  */
-function recognizedAlgorithm (hashBuf) {
-  return hashBuf.includes('$argon2i$') || hashBuf.includes('$argon2id$')
-}
+const recognizedAlgorithm = hashBuf => hashBuf.includes('$argon2i$') || hashBuf.includes('$argon2id$')
 
 /**
  * @typedef SecurePasswordOptions
- * @property {number} [memlimit] - Memory limit for hashing (default: SecurePassword.MEMLIMIT_DEFAULT)
- * @property {number} [opslimit] - Operations limit for hashing (default: SecurePassword.OPSLIMIT_DEFAULT)
+ * @property {number} [memlimit] - Memory limit for hashing (default: MEMLIMIT_DEFAULT)
+ * @property {number} [opslimit] - Operations limit for hashing (default: OPSLIMIT_DEFAULT)
  */
 
 /**
@@ -29,23 +48,26 @@ function recognizedAlgorithm (hashBuf) {
  * @class
  */
 export class SecurePassword {
+  #memlimit
+  #opslimit
+
   /**
    * @param {SecurePasswordOptions} [opts]
    */
   constructor (opts = {}) {
     const {
-      memlimit = SecurePassword.MEMLIMIT_DEFAULT,
-      opslimit = SecurePassword.OPSLIMIT_DEFAULT,
+      memlimit = MEMLIMIT_DEFAULT,
+      opslimit = OPSLIMIT_DEFAULT,
     } = opts
 
-    assert(memlimit >= SecurePassword.MEMLIMIT_MIN, 'opts.memlimit must be at least MEMLIMIT_MIN (' + SecurePassword.MEMLIMIT_MIN + ')')
-    assert(memlimit <= SecurePassword.MEMLIMIT_MAX, 'opts.memlimit must be at most MEMLIMIT_MAX (' + SecurePassword.MEMLIMIT_MAX + ')')
+    assert(memlimit >= MEMLIMIT_MIN, 'opts.memlimit must be at least MEMLIMIT_MIN (' + MEMLIMIT_MIN + ')')
+    assert(memlimit <= MEMLIMIT_MAX, 'opts.memlimit must be at most MEMLIMIT_MAX (' + MEMLIMIT_MAX + ')')
 
-    assert(opslimit >= SecurePassword.OPSLIMIT_MIN, 'opts.opslimit must be at least OPSLIMIT_MIN (' + SecurePassword.OPSLIMIT_MIN + ')')
-    assert(opslimit <= SecurePassword.OPSLIMIT_MAX, 'opts.opslimit must be at most OPSLIMIT_MAX (' + SecurePassword.OPSLIMIT_MAX + ')')
+    assert(opslimit >= OPSLIMIT_MIN, 'opts.opslimit must be at least OPSLIMIT_MIN (' + OPSLIMIT_MIN + ')')
+    assert(opslimit <= OPSLIMIT_MAX, 'opts.opslimit must be at most OPSLIMIT_MAX (' + OPSLIMIT_MAX + ')')
 
-    this.memlimit = memlimit
-    this.opslimit = opslimit
+    this.#memlimit = memlimit
+    this.#opslimit = opslimit
   }
 
   /**
@@ -56,12 +78,12 @@ export class SecurePassword {
    */
   hashSync (passwordBuf) {
     assert(Buffer.isBuffer(passwordBuf), 'passwordBuf must be Buffer')
-    assert(passwordBuf.length >= SecurePassword.PASSWORD_BYTES_MIN, 'passwordBuf must be at least PASSWORD_BYTES_MIN (' + SecurePassword.PASSWORD_BYTES_MIN + ')')
-    assert(passwordBuf.length < SecurePassword.PASSWORD_BYTES_MAX, 'passwordBuf must be shorter than PASSWORD_BYTES_MAX (' + SecurePassword.PASSWORD_BYTES_MAX + ')')
+    assert(passwordBuf.length >= PASSWORD_BYTES_MIN, 'passwordBuf must be at least PASSWORD_BYTES_MIN (' + PASSWORD_BYTES_MIN + ')')
+    assert(passwordBuf.length < PASSWORD_BYTES_MAX, 'passwordBuf must be shorter than PASSWORD_BYTES_MAX (' + PASSWORD_BYTES_MAX + ')')
 
     // Unsafe is okay here since sodium will overwrite all bytes
-    const hashBuf = Buffer.allocUnsafe(SecurePassword.HASH_BYTES)
-    sodium.crypto_pwhash_str(hashBuf, passwordBuf, this.opslimit, this.memlimit)
+    const hashBuf = Buffer.allocUnsafe(HASH_BYTES)
+    sodium.crypto_pwhash_str(hashBuf, passwordBuf, this.#opslimit, this.#memlimit)
 
     // Note that this buffer may have trailing NULL bytes, which is by design
     // (of libsodium). The trailing NULL bytes can be safely trimmed if need
@@ -77,13 +99,13 @@ export class SecurePassword {
    */
   async hash (passwordBuf) {
     assert(Buffer.isBuffer(passwordBuf), 'passwordBuf must be Buffer')
-    assert(passwordBuf.length >= SecurePassword.PASSWORD_BYTES_MIN, 'passwordBuf must be at least PASSWORD_BYTES_MIN (' + SecurePassword.PASSWORD_BYTES_MIN + ')')
-    assert(passwordBuf.length < SecurePassword.PASSWORD_BYTES_MAX, 'passwordBuf must be shorter than PASSWORD_BYTES_MAX (' + SecurePassword.PASSWORD_BYTES_MAX + ')')
+    assert(passwordBuf.length >= PASSWORD_BYTES_MIN, 'passwordBuf must be at least PASSWORD_BYTES_MIN (' + PASSWORD_BYTES_MIN + ')')
+    assert(passwordBuf.length < PASSWORD_BYTES_MAX, 'passwordBuf must be shorter than PASSWORD_BYTES_MAX (' + PASSWORD_BYTES_MAX + ')')
 
     // Unsafe is okay here since sodium will overwrite all bytes
-    const hashBuf = Buffer.allocUnsafe(SecurePassword.HASH_BYTES)
+    const hashBuf = Buffer.allocUnsafe(HASH_BYTES)
 
-    await cryptoPwhashStrAsync(hashBuf, passwordBuf, this.opslimit, this.memlimit)
+    await cryptoPwhashStrAsync(hashBuf, passwordBuf, this.#opslimit, this.#memlimit)
 
     return hashBuf
   }
@@ -93,27 +115,29 @@ export class SecurePassword {
    *
    * @param {Buffer} passwordBuf - The password as a Buffer.
    * @param {Buffer} hashBuf - The hash as a Buffer.
-   * @returns {symbol} One of SecurePassword.INVALID, VALID, VALID_NEEDS_REHASH, or INVALID_UNRECOGNIZED_HASH.
+   * @returns {VerificationResult} One of INVALID, VALID, VALID_NEEDS_REHASH, or INVALID_UNRECOGNIZED_HASH.
    */
   verifySync (passwordBuf, hashBuf) {
     assert(Buffer.isBuffer(passwordBuf), 'passwordBuf must be Buffer')
-    assert(passwordBuf.length >= SecurePassword.PASSWORD_BYTES_MIN, 'passwordBuf must be at least PASSWORD_BYTES_MIN (' + SecurePassword.PASSWORD_BYTES_MIN + ')')
-    assert(passwordBuf.length < SecurePassword.PASSWORD_BYTES_MAX, 'passwordBuf must be shorter than PASSWORD_BYTES_MAX (' + SecurePassword.PASSWORD_BYTES_MAX + ')')
+    assert(passwordBuf.length >= PASSWORD_BYTES_MIN, 'passwordBuf must be at least PASSWORD_BYTES_MIN (' + PASSWORD_BYTES_MIN + ')')
+    assert(passwordBuf.length < PASSWORD_BYTES_MAX, 'passwordBuf must be shorter than PASSWORD_BYTES_MAX (' + PASSWORD_BYTES_MAX + ')')
 
     assert(Buffer.isBuffer(hashBuf), 'hashBuf must be Buffer')
-    assert(hashBuf.length === SecurePassword.HASH_BYTES, 'hashBuf must be HASH_BYTES (' + SecurePassword.HASH_BYTES + ')')
+    assert(hashBuf.length === HASH_BYTES, 'hashBuf must be HASH_BYTES (' + HASH_BYTES + ')')
 
-    if (recognizedAlgorithm(hashBuf) === false) return SecurePassword.INVALID_UNRECOGNIZED_HASH
+    if (recognizedAlgorithm(hashBuf) === false) {
+      return INVALID_UNRECOGNIZED_HASH
+    }
 
     if (sodium.crypto_pwhash_str_verify(hashBuf, passwordBuf) === false) {
-      return SecurePassword.INVALID
+      return INVALID
     }
 
-    if (sodium.crypto_pwhash_str_needs_rehash(hashBuf, this.opslimit, this.memlimit)) {
-      return SecurePassword.VALID_NEEDS_REHASH
+    if (sodium.crypto_pwhash_str_needs_rehash(hashBuf, this.#opslimit, this.#memlimit)) {
+      return VALID_NEEDS_REHASH
     }
 
-    return SecurePassword.VALID
+    return VALID
   }
 
   /**
@@ -121,56 +145,29 @@ export class SecurePassword {
    *
    * @param {Buffer} passwordBuf - The password as a Buffer.
    * @param {Buffer} hashBuf - The hash as a Buffer.
-   * @returns {Promise<symbol>} Promise if no callback is given.
+   * @returns {Promise<VerificationResult>} Promise if no callback is given.
    */
   async verify (passwordBuf, hashBuf) {
     assert(Buffer.isBuffer(passwordBuf), 'passwordBuf must be Buffer')
-    assert(passwordBuf.length >= SecurePassword.PASSWORD_BYTES_MIN, 'passwordBuf must be at least PASSWORD_BYTES_MIN (' + SecurePassword.PASSWORD_BYTES_MIN + ')')
-    assert(passwordBuf.length < SecurePassword.PASSWORD_BYTES_MAX, 'passwordBuf must be shorter than PASSWORD_BYTES_MAX (' + SecurePassword.PASSWORD_BYTES_MAX + ')')
+    assert(passwordBuf.length >= PASSWORD_BYTES_MIN, 'passwordBuf must be at least PASSWORD_BYTES_MIN (' + PASSWORD_BYTES_MIN + ')')
+    assert(passwordBuf.length < PASSWORD_BYTES_MAX, 'passwordBuf must be shorter than PASSWORD_BYTES_MAX (' + PASSWORD_BYTES_MAX + ')')
 
     assert(Buffer.isBuffer(hashBuf), 'hashBuf must be Buffer')
-    assert(hashBuf.length === SecurePassword.HASH_BYTES, 'hashBuf must be HASH_BYTES (' + SecurePassword.HASH_BYTES + ')')
+    assert(hashBuf.length === HASH_BYTES, 'hashBuf must be HASH_BYTES (' + HASH_BYTES + ')')
 
     if (recognizedAlgorithm(hashBuf) === false) {
-      return SecurePassword.INVALID_UNRECOGNIZED_HASH
+      return INVALID_UNRECOGNIZED_HASH
     }
 
     const bool = await cryptoPwhashStrVerifyAsync(hashBuf, passwordBuf)
 
     if (bool === false) {
-      return SecurePassword.INVALID
+      return INVALID
     }
-    if (cryptoPwhashStrNeedsRehash(hashBuf, this.opslimit, this.memlimit)) {
-      return SecurePassword.VALID_NEEDS_REHASH
+    if (cryptoPwhashStrNeedsRehash(hashBuf, this.#opslimit, this.#memlimit)) {
+      return VALID_NEEDS_REHASH
     }
 
-    return SecurePassword.VALID
+    return VALID
   }
-
-  /** @type {number} */
-  static HASH_BYTES = sodium.crypto_pwhash_STRBYTES
-  /** @type {number} */
-  static PASSWORD_BYTES_MIN = sodium.crypto_pwhash_PASSWD_MIN
-  /** @type {number} */
-  static PASSWORD_BYTES_MAX = sodium.crypto_pwhash_PASSWD_MAX
-  /** @type {number} */
-  static MEMLIMIT_MIN = sodium.crypto_pwhash_MEMLIMIT_MIN
-  /** @type {number} */
-  static MEMLIMIT_MAX = sodium.crypto_pwhash_MEMLIMIT_MAX
-  /** @type {number} */
-  static OPSLIMIT_MIN = sodium.crypto_pwhash_OPSLIMIT_MIN
-  /** @type {number} */
-  static OPSLIMIT_MAX = sodium.crypto_pwhash_OPSLIMIT_MAX
-  /** @type {number} */
-  static MEMLIMIT_DEFAULT = sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE
-  /** @type {number} */
-  static OPSLIMIT_DEFAULT = sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE
-  /** @type {symbol} */
-  static INVALID_UNRECOGNIZED_HASH = Symbol('INVALID_UNRECOGNIZED_HASH')
-  /** @type {symbol} */
-  static INVALID = Symbol('INVALID')
-  /** @type {symbol} */
-  static VALID = Symbol('VALID')
-  /** @type {symbol} */
-  static VALID_NEEDS_REHASH = Symbol('VALID_NEEDS_REHASH')
 }
--- test.js
+++ test.js
@@ -1,14 +1,28 @@
 /* eslint-disable n/no-sync */
 
 import test from 'node:test'
-import { SecurePassword } from './index.js'
+
+import {
+  HASH_BYTES,
+  INVALID,
+  INVALID_UNRECOGNIZED_HASH,
+  MEMLIMIT_DEFAULT,
+  MEMLIMIT_MIN,
+  OPSLIMIT_DEFAULT,
+  OPSLIMIT_MIN,
+  PASSWORD_BYTES_MAX,
+  PASSWORD_BYTES_MIN,
+  SecurePassword,
+  VALID,
+  VALID_NEEDS_REHASH,
+} from './index.js'
 
 /** @import { TestContext } from 'node:test' */
 
 test('Can hash password sync', /** @param {TestContext} t */ (t) => {
   const pwd = new SecurePassword({
-    memlimit: SecurePassword.MEMLIMIT_DEFAULT,
-    opslimit: SecurePassword.OPSLIMIT_DEFAULT,
+    memlimit: MEMLIMIT_DEFAULT,
+    opslimit: OPSLIMIT_DEFAULT,
   })
 
   const userPassword = Buffer.from('my secrets')
@@ -19,8 +33,8 @@ test('Can hash password sync', /** @param {TestContext} t */ (t) => {
 
 test('Can hash password using promises', /** @param {TestContext} t */ async (t) => {
   const pwd = new SecurePassword({
-    memlimit: SecurePassword.MEMLIMIT_DEFAULT,
-    opslimit: SecurePassword.OPSLIMIT_DEFAULT,
+    memlimit: MEMLIMIT_DEFAULT,
+    opslimit: OPSLIMIT_DEFAULT,
   })
 
   const userPassword = Buffer.from('my secrets')
@@ -32,8 +46,8 @@ test('Can hash password using promises', /** @param {TestContext} t */ async (t)
 test('Can hash password async simultaneous', /** @param {TestContext} t */ async (t) => {
   t.plan(4)
   const pwd = new SecurePassword({
-    memlimit: SecurePassword.MEMLIMIT_DEFAULT,
-    opslimit: SecurePassword.OPSLIMIT_DEFAULT,
+    memlimit: MEMLIMIT_DEFAULT,
+    opslimit: OPSLIMIT_DEFAULT,
   })
 
   const userPassword = Buffer.from('my secrets')
@@ -50,33 +64,33 @@ test('Can hash password async simultaneous', /** @param {TestContext} t */ async
 
 test('Can verify password (identity) sync', /** @param {TestContext} t */ (t) => {
   const pwd = new SecurePassword({
-    memlimit: SecurePassword.MEMLIMIT_DEFAULT,
-    opslimit: SecurePassword.OPSLIMIT_DEFAULT,
+    memlimit: MEMLIMIT_DEFAULT,
+    opslimit: OPSLIMIT_DEFAULT,
   })
 
   const userPassword = Buffer.from('my secret')
   const passwordHash = pwd.hashSync(userPassword)
 
-  t.assert.strictEqual(pwd.verifySync(userPassword, passwordHash), SecurePassword.VALID)
+  t.assert.strictEqual(pwd.verifySync(userPassword, passwordHash), VALID)
 })
 
 test('Can verify password (identity) using promises', /** @param {TestContext} t */ async (t) => {
   const pwd = new SecurePassword({
-    memlimit: SecurePassword.MEMLIMIT_DEFAULT,
-    opslimit: SecurePassword.OPSLIMIT_DEFAULT,
+    memlimit: MEMLIMIT_DEFAULT,
+    opslimit: OPSLIMIT_DEFAULT,
   })
 
   const userPassword = Buffer.from('my secret')
   const passwordHash = await pwd.hash(userPassword)
   const bool = await pwd.verify(userPassword, passwordHash)
 
-  t.assert.strictEqual(bool, SecurePassword.VALID)
+  t.assert.strictEqual(bool, VALID)
 })
 
 test('Needs rehash sync', /** @param {TestContext} t */ (t) => {
   const weakPwd = new SecurePassword({
-    memlimit: SecurePassword.MEMLIMIT_DEFAULT,
-    opslimit: SecurePassword.OPSLIMIT_DEFAULT,
+    memlimit: MEMLIMIT_DEFAULT,
+    opslimit: OPSLIMIT_DEFAULT,
   })
 
   const userPassword = Buffer.from('my secret')
@@ -88,79 +102,79 @@ test('Needs rehash sync', /** @param {TestContext} t */ (t) => {
   const weakHash = weakPwd.hashSync(userPassword)
   const weakValid = weakPwd.verifySync(userPassword, weakHash)
 
-  t.assert.strictEqual(weakValid, SecurePassword.VALID)
-  t.assert.notStrictEqual(weakValid, SecurePassword.INVALID)
-  t.assert.notStrictEqual(weakValid, SecurePassword.VALID_NEEDS_REHASH)
+  t.assert.strictEqual(weakValid, VALID)
+  t.assert.notStrictEqual(weakValid, INVALID)
+  t.assert.notStrictEqual(weakValid, VALID_NEEDS_REHASH)
 
   const weakInvalid = weakPwd.verifySync(wrongPassword, weakHash)
 
-  t.assert.notStrictEqual(weakInvalid, SecurePassword.VALID)
-  t.assert.strictEqual(weakInvalid, SecurePassword.INVALID)
-  t.assert.notStrictEqual(weakInvalid, SecurePassword.VALID_NEEDS_REHASH)
+  t.assert.notStrictEqual(weakInvalid, VALID)
+  t.assert.strictEqual(weakInvalid, INVALID)
+  t.assert.notStrictEqual(weakInvalid, VALID_NEEDS_REHASH)
 
   const betterPwd = new SecurePassword({
-    memlimit: SecurePassword.MEMLIMIT_DEFAULT + 1024,
-    opslimit: SecurePassword.OPSLIMIT_DEFAULT + 1,
+    memlimit: MEMLIMIT_DEFAULT + 1024,
+    opslimit: OPSLIMIT_DEFAULT + 1,
   })
 
   const rehashValid = betterPwd.verifySync(userPassword, weakHash)
 
-  t.assert.notStrictEqual(rehashValid, SecurePassword.VALID)
-  t.assert.notStrictEqual(rehashValid, SecurePassword.INVALID)
-  t.assert.strictEqual(rehashValid, SecurePassword.VALID_NEEDS_REHASH)
+  t.assert.notStrictEqual(rehashValid, VALID)
+  t.assert.notStrictEqual(rehashValid, INVALID)
+  t.assert.strictEqual(rehashValid, VALID_NEEDS_REHASH)
 
   const rehashValidAlgo = betterPwd.verifySync(pass, argon2ipass)
 
-  t.assert.notStrictEqual(rehashValidAlgo, SecurePassword.VALID)
-  t.assert.notStrictEqual(rehashValidAlgo, SecurePassword.INVALID)
-  t.assert.strictEqual(rehashValidAlgo, SecurePassword.VALID_NEEDS_REHASH)
+  t.assert.notStrictEqual(rehashValidAlgo, VALID)
+  t.assert.notStrictEqual(rehashValidAlgo, INVALID)
+  t.assert.strictEqual(rehashValidAlgo, VALID_NEEDS_REHASH)
 
   const rehashValidAlgoEmpty = betterPwd.verifySync(empty, argon2ipassempty)
 
-  t.assert.notStrictEqual(rehashValidAlgoEmpty, SecurePassword.VALID)
-  t.assert.notStrictEqual(rehashValidAlgoEmpty, SecurePassword.INVALID)
-  t.assert.strictEqual(rehashValidAlgoEmpty, SecurePassword.VALID_NEEDS_REHASH)
+  t.assert.notStrictEqual(rehashValidAlgoEmpty, VALID)
+  t.assert.notStrictEqual(rehashValidAlgoEmpty, INVALID)
+  t.assert.strictEqual(rehashValidAlgoEmpty, VALID_NEEDS_REHASH)
 
   const betterHash = betterPwd.hashSync(userPassword)
   const betterValid = betterPwd.verifySync(userPassword, betterHash)
 
-  t.assert.strictEqual(betterValid, SecurePassword.VALID)
-  t.assert.notStrictEqual(betterValid, SecurePassword.INVALID)
-  t.assert.notStrictEqual(betterValid, SecurePassword.VALID_NEEDS_REHASH)
+  t.assert.strictEqual(betterValid, VALID)
+  t.assert.notStrictEqual(betterValid, INVALID)
+  t.assert.notStrictEqual(betterValid, VALID_NEEDS_REHASH)
 
   const betterInvalid = betterPwd.verifySync(wrongPassword, betterHash)
 
-  t.assert.notStrictEqual(betterInvalid, SecurePassword.VALID)
-  t.assert.strictEqual(betterInvalid, SecurePassword.INVALID)
-  t.assert.notStrictEqual(betterInvalid, SecurePassword.VALID_NEEDS_REHASH)
+  t.assert.notStrictEqual(betterInvalid, VALID)
+  t.assert.strictEqual(betterInvalid, INVALID)
+  t.assert.notStrictEqual(betterInvalid, VALID_NEEDS_REHASH)
 })
 
 test('Can handle invalid hash sync', /** @param {TestContext} t */ (t) => {
   const pwd = new SecurePassword()
 
   const userPassword = Buffer.from('my secret')
-  const invalidHash = Buffer.allocUnsafe(SecurePassword.HASH_BYTES)
+  const invalidHash = Buffer.allocUnsafe(HASH_BYTES)
 
   const unrecognizedHash = pwd.verifySync(userPassword, invalidHash)
 
-  t.assert.strictEqual(unrecognizedHash, SecurePassword.INVALID_UNRECOGNIZED_HASH)
-  t.assert.notStrictEqual(unrecognizedHash, SecurePassword.INVALID)
-  t.assert.notStrictEqual(unrecognizedHash, SecurePassword.VALID)
-  t.assert.notStrictEqual(unrecognizedHash, SecurePassword.VALID_NEEDS_REHASH)
+  t.assert.strictEqual(unrecognizedHash, INVALID_UNRECOGNIZED_HASH)
+  t.assert.notStrictEqual(unrecognizedHash, INVALID)
+  t.assert.notStrictEqual(unrecognizedHash, VALID)
+  t.assert.notStrictEqual(unrecognizedHash, VALID_NEEDS_REHASH)
 })
 
 test('Can handle invalid hash async', /** @param {TestContext} t */ async (t) => {
   const pwd = new SecurePassword()
 
   const userPassword = Buffer.from('my secret')
-  const invalidHash = Buffer.allocUnsafe(SecurePassword.HASH_BYTES)
+  const invalidHash = Buffer.allocUnsafe(HASH_BYTES)
 
   const unrecognizedHash = await pwd.verify(userPassword, invalidHash)
 
-  t.assert.strictEqual(unrecognizedHash, SecurePassword.INVALID_UNRECOGNIZED_HASH)
-  t.assert.notStrictEqual(unrecognizedHash, SecurePassword.INVALID)
-  t.assert.notStrictEqual(unrecognizedHash, SecurePassword.VALID)
-  t.assert.notStrictEqual(unrecognizedHash, SecurePassword.VALID_NEEDS_REHASH)
+  t.assert.strictEqual(unrecognizedHash, INVALID_UNRECOGNIZED_HASH)
+  t.assert.notStrictEqual(unrecognizedHash, INVALID)
+  t.assert.notStrictEqual(unrecognizedHash, VALID)
+  t.assert.notStrictEqual(unrecognizedHash, VALID_NEEDS_REHASH)
 })
 
 test('Verify async returns INVALID if password is wrong', /** @param {TestContext} t */ async (t) => {
@@ -172,14 +186,14 @@ test('Verify async returns INVALID if password is wrong', /** @param {TestContex
   const passwordHash = await pwd.hash(userPassword)
   const result = await pwd.verify(wrongPassword, passwordHash)
 
-  t.assert.strictEqual(result, SecurePassword.INVALID)
+  t.assert.strictEqual(result, INVALID)
 })
 
 test('Verify async returns VALID_NEEDS_REHASH if hash needs rehash', /** @param {TestContext} t */ async (t) => {
   // Create a hash with intentionally lower (weak) parameters...
   const weakPwd = new SecurePassword({
-    memlimit: SecurePassword.MEMLIMIT_MIN, // intentionally as low as allowed
-    opslimit: SecurePassword.OPSLIMIT_MIN, // intentionally as low as allowed
+    memlimit: MEMLIMIT_MIN, // intentionally as low as allowed
+    opslimit: OPSLIMIT_MIN, // intentionally as low as allowed
   })
 
   const userPassword = Buffer.from('my secret')
@@ -187,13 +201,13 @@ test('Verify async returns VALID_NEEDS_REHASH if hash needs rehash', /** @param 
 
   // ...then verify with stricter (stronger) params
   const betterPwd = new SecurePassword({
-    memlimit: SecurePassword.MEMLIMIT_DEFAULT + 1024, // higher than default
-    opslimit: SecurePassword.OPSLIMIT_DEFAULT + 1,    // higher than default
+    memlimit: MEMLIMIT_DEFAULT + 1024, // higher than default
+    opslimit: OPSLIMIT_DEFAULT + 1,    // higher than default
   })
 
   const result = await betterPwd.verify(userPassword, weakHash)
 
-  t.assert.strictEqual(result, SecurePassword.VALID_NEEDS_REHASH)
+  t.assert.strictEqual(result, VALID_NEEDS_REHASH)
 })
 
 // *** Assert tests ***
@@ -202,17 +216,17 @@ test('Verify async throws if passwordBuf is not a Buffer', async (t) => {
   const pwd = new SecurePassword()
   await t.assert.rejects(
     // @ts-expect-error: Intentionally passing a string to test runtime assertion for non-Buffer input
-    () => pwd.verify('not-a-buffer', Buffer.alloc(SecurePassword.HASH_BYTES)),
+    () => pwd.verify('not-a-buffer', Buffer.alloc(HASH_BYTES)),
     /passwordBuf must be Buffer/
   )
 })
 
 test('Verify async throws if passwordBuf is too short', async (t) => {
-  const min = Math.max(0, SecurePassword.PASSWORD_BYTES_MIN - 1)
+  const min = Math.max(0, PASSWORD_BYTES_MIN - 1)
   if (min > 0) {
     const pwd = new SecurePassword()
     await t.assert.rejects(
-      () => pwd.verify(Buffer.alloc(min), Buffer.alloc(SecurePassword.HASH_BYTES)),
+      () => pwd.verify(Buffer.alloc(min), Buffer.alloc(HASH_BYTES)),
       /passwordBuf must be at least PASSWORD_BYTES_MIN/
     )
   }
@@ -221,7 +235,7 @@ test('Verify async throws if passwordBuf is too short', async (t) => {
 test('Verify async throws if passwordBuf is too long', async (t) => {
   const pwd = new SecurePassword()
   await t.assert.rejects(
-    () => pwd.verify(Buffer.alloc(SecurePassword.PASSWORD_BYTES_MAX + 1), Buffer.alloc(SecurePassword.HASH_BYTES)),
+    () => pwd.verify(Buffer.alloc(PASSWORD_BYTES_MAX + 1), Buffer.alloc(HASH_BYTES)),
     /passwordBuf must be shorter than PASSWORD_BYTES_MAX/
   )
 })
@@ -230,7 +244,7 @@ test('Verify async throws if hashBuf is not a Buffer', async (t) => {
   const pwd = new SecurePassword()
   await t.assert.rejects(
     // @ts-expect-error: Intentionally passing a string as hashBuf to test runtime assertion for non-Buffer input
-    () => pwd.verify(Buffer.alloc(SecurePassword.PASSWORD_BYTES_MIN), 'not-a-buffer'),
+    () => pwd.verify(Buffer.alloc(PASSWORD_BYTES_MIN), 'not-a-buffer'),
     /hashBuf must be Buffer/
   )
 })
@@ -238,7 +252,7 @@ test('Verify async throws if hashBuf is not a Buffer', async (t) => {
 test('Verify async throws if hashBuf is wrong length', async (t) => {
   const pwd = new SecurePassword()
   await t.assert.rejects(
-    () => pwd.verify(Buffer.alloc(SecurePassword.PASSWORD_BYTES_MIN), Buffer.alloc(1)),
+    () => pwd.verify(Buffer.alloc(PASSWORD_BYTES_MIN), Buffer.alloc(1)),
     /hashBuf must be HASH_BYTES/
   )
 })
